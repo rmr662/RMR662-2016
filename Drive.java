ad21cb3e931db25905b5fce4cf3591f0c58ac5a8
jkrmnj <jkrmnj465@gmail.com>
2016-01-13 19:30:13 -0700

    Architecture stuff

---
+++ Drive.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team662.robot;
+
+public class Drive extends Component{
+	
+	public Drive(){
+		
+	}
+	
+	public void autoUpdate(){
+		
+	}
+	
+	public void update(){
+		
+	}
+}

6cbb05f0d8e5d450328d52b61cf6366fd98d0685
= <=>
2016-01-18 13:43:17 -0700

    simple drive

--- Drive.java
+++ Drive.java
@@ -1,9 +1,15 @@
 package org.usfirst.frc.team662.robot;
-
+import edu.wpi.first.wpilibj.RobotDrive;
 public class Drive extends Component{
+	RobotDrive driver; 
+	DualTalon left;
+	DualTalon right; 
 	
 	public Drive(){
-		
+		left = new DualTalon(3,6);
+		right = new DualTalon(4,5);
+				
+		driver = new RobotDrive(left,right);
 	}
 	
 	public void autoUpdate(){
@@ -11,6 +17,8 @@ public class Drive extends Component{
 	}
 	
 	public void update(){
-		
+		driver.arcadeDrive(Robot.stick.getRawAxis(1), Robot.stick.getRawAxis(4));
+	
 	}
 }
+

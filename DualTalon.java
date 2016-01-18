5219d86db784e8bf4e7af3df99cd9441f44531bb
= <=>
2016-01-18 12:37:02 -0700

    Added DualTalon class

---
+++ DualTalon.java
@@ -0,0 +1,109 @@
+package org.usfirst.frc.team662.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class DualTalon implements SpeedController {
+	
+	TalonSRX left, right;
+	
+	double currentSpeed;
+	double leftSpeedMultiplier;
+	double rightSpeedMultiplier;
+	
+	public DualTalon(int leftChannel, int rightChannel) {
+		this(new TalonSRX(leftChannel), new TalonSRX(rightChannel));
+	}
+	
+	public DualTalon(TalonSRX left, TalonSRX right) {
+		this.left = left;
+		this.right = right;
+		
+		currentSpeed = 0;
+		leftSpeedMultiplier = rightSpeedMultiplier = 1.0;
+	}
+	
+	public void setMultiplier(double multiplier) {
+		setMultiplier(true, multiplier);
+		setMultiplier(false, multiplier);
+	}
+	
+	public void setMultiplier(boolean left, double multiplier) {
+		if (multiplier > 1.0) {
+			multiplier = 1.0;
+		}
+		else if (multiplier < -1.0) {
+			multiplier = -1.0;
+		}
+		
+		if (left) {
+			leftSpeedMultiplier = multiplier;
+		}
+		else {
+			rightSpeedMultiplier = multiplier;
+		}
+	}
+	
+	public void invert() {
+		invert(true);
+		invert(false);
+	}
+	
+	public void invert(boolean left) {
+		if (left) {
+			leftSpeedMultiplier = -leftSpeedMultiplier;
+		}
+		else {
+			rightSpeedMultiplier = -rightSpeedMultiplier;
+		}
+	}
+
+	@Override
+	public void pidWrite(double output) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public double get() {
+		return currentSpeed;
+	}
+	
+	// Does nothing
+	@Override
+	public void set(double speed, byte syncGroup) {
+		set(speed);
+	}
+
+	@Override
+	public void set(double speed) {
+		// TODO Auto-generated method stub
+		if (speed < -1) {
+			speed = -1;
+		}
+		else if (speed > 1) {
+			speed = 1;
+		}
+		
+		currentSpeed = speed;
+		
+		left.set(leftSpeedMultiplier * currentSpeed);
+		right.set(rightSpeedMultiplier * currentSpeed);
+	}
+
+	// Does nothing
+	@Override
+	public void setInverted(boolean isInverted) {
+		
+	}
+
+	// Does nothing
+	@Override
+	public boolean getInverted() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void disable() {
+		leftSpeedMultiplier = rightSpeedMultiplier = 0.0;
+	}
+}

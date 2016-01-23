380937332ee5147d38b656f60f450c39496b6c7e
= <=>
2016-01-22 20:31:36 -0700

    Functional drive code added

--- Drive.java
+++ Drive.java
@@ -1,16 +1,21 @@
 package org.usfirst.frc.team662.robot;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.RobotDrive;
 public class Drive extends Component{
 	RobotDrive driver; 
 	DualTalon left;
 	DualTalon right; 
+	final static double LEFT_MULTIPLIER = -0.3;
+	final static double RIGHT_MULTIPLIER = 0.3;
+	final static double LEFT_DEADZONE = 0.1;
+	final static double RIGHT_DEADZONE = 0.1;
 	
 	public Drive(){
 		left = new DualTalon(3,6);
 		right = new DualTalon(4,5);
 		
-		left.setMultiplier(.2);
-		right.setMultiplier(.2);
+		left.setMultiplier(LEFT_MULTIPLIER);
+		right.setMultiplier(RIGHT_MULTIPLIER);
 				
 		driver = new RobotDrive(left,right);
 	}
@@ -20,8 +25,64 @@ public class Drive extends Component{
 	}
 	
 	public void update(){
-		driver.arcadeDrive(Robot.stick.getRawAxis(XboxMap.LEFT_JOY_VERT), Robot.stick.getRawAxis(XboxMap.RIGHT_JOY_HORIZ));
+		//experimental drive train code by James S.
+    	
+           
+       //set variables to defaults
+       double rightMotorPower = 0;
+       double leftMotorPower = 0;
+       double highInput = 1;
+       double leftInput = -Robot.stick.getRawAxis(XboxMap.LEFT_JOY_VERT);
+       double rightInput = Robot.stick.getRawAxis(XboxMap.RIGHT_JOY_HORIZ);
+       
+       //set motor power variables to left stick value
+       if(Math.abs(leftInput) > LEFT_DEADZONE){
+    	   leftMotorPower = leftInput;
+    	   rightMotorPower = leftInput;
+       }
+       
+       //modify motor power variables by right stick values
+       if(Math.abs(rightInput) > RIGHT_DEADZONE){
+    	   leftMotorPower = leftMotorPower + rightInput;
+    	   rightMotorPower = rightMotorPower - rightInput;
+       }
+       
+       //determine the highest input received
+       highInput = findHighInput(leftInput, rightInput);
+       
+       //limit left motor power to highest input received
+       leftMotorPower = limitMotor(leftMotorPower, highInput);
+       
+       //limit right motor power to highest input received
+       rightMotorPower = limitMotor(rightMotorPower, highInput);
+       
+       //set the motors to power variables
+       left.set(leftMotorPower);
+       right.set(rightMotorPower);
 	
 	}
+	
+	//determines which of two inputs is greater, and returns it
+	public double findHighInput(double input1, double input2){
+		double resultInput = 0;
+        if(Math.abs(input1) >= Math.abs(input2)){
+    	    resultInput = input1;
+        }else{
+    	    resultInput = input2;
+        }
+		return resultInput;
+	}
+	
+	//limits a motor power value to a limiting value
+	public double limitMotor(double inputPower, double limiter){
+		double outputPower = inputPower;
+		if(Math.abs(inputPower) > Math.abs(limiter)){
+	    	   if(inputPower < 0){
+	    		   outputPower = -Math.abs(limiter);
+	    	   }else{
+	    		   outputPower = Math.abs(limiter);
+	    	   }
+	       }
+		return outputPower;
+	}
 }
-

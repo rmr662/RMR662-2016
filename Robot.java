3fda12e404bb4caa2b1eb60cdfc7622ae240fe93
jkrmnj <jkrmnj465@gmail.com>
2016-02-05 19:36:54 -0700

    Added ball grabber

--- Robot.java
+++ Robot.java
@@ -15,7 +15,7 @@ public class Robot extends SampleRobot {
     public static Joystick manStick;
 
     Component[] parts;
-    static final int NUM_PARTS = 5;
+    static final int NUM_PARTS = 6;
 
     public Robot() {
         parts = new Component[NUM_PARTS];
@@ -24,6 +24,7 @@ public class Robot extends SampleRobot {
         parts[2] = new Shifter();
         parts[3] = new Arm();
         parts[4] = new BackArm();
+        parts[5] = new BallGrabber();
         stick = new Joystick(0);
         manStick = new Joystick(1);
     }

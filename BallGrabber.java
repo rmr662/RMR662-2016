2897f00cf9d4662181bf78cfe17cbe3a8f85ed6a
jkrmnj <jkrmnj465@gmail.com>
2016-02-13 11:21:25 -0700

    More ready for testing

--- BallGrabber.java
+++ BallGrabber.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team662.robot;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Talon;
 
 public class BallGrabber extends Component{
@@ -12,6 +11,7 @@ public class BallGrabber extends Component{
 	final static double SPEED_BACKWARD = -1;
 	boolean clockwise = false;
 	boolean runBefore = false;
+	boolean limitHit = false;
 	
 	public BallGrabber(){
 		grabbingTalon = new Talon(MOTORPORT);
@@ -20,8 +20,12 @@ public class BallGrabber extends Component{
 	public void update(){
 		boolean buttonPressed;
 		buttonPressed = Robot.manipulator.getRawButton(XboxMap.LB);
-		if(buttonOn.get()){
+		if(buttonOn.get() && limitHit == false){
 			grabbingTalon.set(0);
+			limitHit = true;
+		}
+		if(!buttonOn.get()){
+			limitHit = false;
 		}
 	
 		if(buttonPressed && !runBefore){	
@@ -34,6 +38,7 @@ public class BallGrabber extends Component{
 				grabbingTalon.set(SPEED_BACKWARD);
 			}
 			runBefore = true;
+			
 		}
 		else{
 			if(buttonPressed){

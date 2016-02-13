9bdafc573cf6b5f43bb9d4d8efb37e892db70952
jkrmnj <jkrmnj465@gmail.com>
2016-02-12 18:29:41 -0700

    Fixed Sam 's ballgrabber

--- BallGrabber.java
+++ BallGrabber.java
@@ -2,21 +2,22 @@ package org.usfirst.frc.team662.robot;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.CANTalon;
 public class BallGrabber extends Component{
- CANTalon grabbingTalon;
- final static int MOTORPORT = 1;
- final static int SWITCHPORT = 1;
- DigitalInput buttonOn;
- final static double SPEED_FORWARD = 1;
- final static double SPEED_BACKWARD = -1;
- boolean clockwise = false;
- boolean runBefore = false;
+	CANTalon grabbingTalon;
+	final static int MOTORPORT = 1;
+	final static int SWITCHPORT = 1;
+	DigitalInput buttonOn;
+	final static double SPEED_FORWARD = 1;
+	final static double SPEED_BACKWARD = -1;
+	boolean clockwise = false;
+	boolean runBefore = false;
+	
 	public BallGrabber(){
 		grabbingTalon = new CANTalon(MOTORPORT);
 		buttonOn = new DigitalInput(SWITCHPORT);
 	}
 	public void update(){
 		boolean buttonPressed;
-		buttonPressed = Robot.manStick.getRawButton(XboxMap.LB);
+		buttonPressed = Robot.manipulator.getRawButton(XboxMap.LB);
 		if(buttonOn.get()){
 			grabbingTalon.set(0);
 		}
